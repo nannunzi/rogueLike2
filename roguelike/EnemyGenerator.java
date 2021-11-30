41a0788fc246725bff081067a21eded79c4ce82c
Daniel Peterson <dapeterson@cox.net>
2021-11-30 17:00:53 +0000

    Fixed EnemyGenerator and added EnemyType so it will work

--- roguelike/EnemyGenerator.java
+++ roguelike/EnemyGenerator.java
@@ -1,65 +1,67 @@
 // this class contains a static method for creating enemies randomly
 import java.util.Random;
 public class EnemyGenerator{
-         private static String[] descriptor =new String[]{"Poor","Old","Mystic","Repulsive","Dazzling","Twisted","Elder","Sluggish","Stinky","Weird"};
-         private static EnemyType[] types = new EnemyType[]{EnemyType.Bruiser, ItemType.Theif, ItemType.Undead};
+	private static String[] descriptor =new String[]{"Poor","Old","Mystic","Repulsive","Dazzling","Twisted","Elder","Sluggish","Stinky","Weird"};
+	private static EnemyType[] types = new EnemyType[]{EnemyType.Bruiser, EnemyType.Thief, EnemyType.Undead};
 
-        public static Enemy generate(){
+	public static Enemy generate(int row, int col){
 		int hp=0;
 		int dam=0;
 		int prot = 0; 
-                Random rng = new Random();
-                double rngprep= rng.nextDouble();
-                rngprep = rngprep*10;
-                int rngint = (int)rngprep;
-                String name= "";
-                name += descriptor[rngint];
-                name += " ";
-                double rngb=rng.nextDouble();
-                rngb=rngb*3;
-                rngint= (int)rngb;
+		Random rng = new Random();
+		double rngprep= rng.nextDouble();
+		rngprep = rngprep*10;
+		int rngint = (int)rngprep;
+		String name= "";
+		name += descriptor[rngint];
+		name += " ";
+		double rngb=rng.nextDouble();
+		rngb=rngb*3;
+		rngint= (int)rngb;
 		double rngc=rng.nextDouble();
-                rngc=rngc*3;
-	        int rngin= (int)rngc;
-                switch (rngin){
-                case 0: name += " Orc";
+		rngc=rngc*3;
+		int rngin= (int)rngc;
+		switch (rngin){
+			case 0: name += " Orc";
 				hp = 60;
 				dam = 15;
 				prot = 3;	
-                                  break;
-                case 1: name += " Goblin";
+				break;
+			case 1: name += " Goblin";
 				hp = 30;
 				dam = 20;
 				prot = 1;
-                                  break;
-                case 2: name += " Pirate";
+				break;
+			case 2: name += " Pirate";
 				hp = 50;
 				dam = 10;
-				prot = 2
-               			  break;
-                EnemyType type = types[rngint];
-                switch (rngint){
-                        case 0: name += " Bruiser";
-				prot += 1;
-				hp+=5;
-                                break;
-                        case 1: name += " Theif";
-                                hp = hp/2;
-				dam += 5;
+				prot = 2;
 				break;
-                        case 2: name += " that died last week";
-                                hp-=10;
-				dam=dam*2;
-				break;
-                        }
-               rngprep=rng.nextDouble();
-               rngprep=rngprep*200;
-               rngint= (int)rngprep;
-               int r = rngint;
-		rngprep = rng.nxtDouble();
-		rngprep=rngprep*500;
-		rngint=(int)rngprep;
-		int c = rngint;
-               return new Enemy(name, r,c, hp, dam, prot);
-       }
 }
+				EnemyType type = types[rngint];
+				switch (rngint){
+					case 0: name += " Bruiser";
+						prot += 1;
+						hp+=5;
+						break;
+					case 1: name += " Thief";
+						hp = hp/2;
+						dam += 5;
+						break;
+					case 2: name += " that died last week";
+						hp-=10;
+						dam=dam*2;
+						break;
+				}
+				rngprep=rng.nextDouble();
+				rngprep=rngprep*200;
+				rngint= (int)rngprep;
+				int r = row;
+				rngprep = rng.nextDouble();
+				rngprep=rngprep*500;
+				rngint=(int)rngprep;
+				int c = col;
+				return new Enemy(name, r,c, hp, dam, prot);
+		}
+	}
+
---
+++ roguelike/EnemyType.java
@@ -0,0 +1,8 @@
+// EnemyType.java
+// an enum for specifying what type an enemy is
+
+public enum EnemyType {
+    Bruiser,
+    Thief,
+    Undead
+}
